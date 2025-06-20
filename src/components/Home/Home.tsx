5d81eb27be96dcf570d6466e94b3a73fb0d97f35
roysreejan <sreejanroy01@gmail.com>
2025-06-20 21:18:01 +0600

    sidebar with logout function completed

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,14 +1,44 @@
 "use client";
 
 import React from "react";
-import { useSelector } from "react-redux";
-import { RootState } from "../../../store/store";
+import LeftSidebar from "./LeftSidebar";
+import Feed from "./Feed";
+import RightSidebar from "./RightSidebar";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetTitle,
+  SheetTrigger,
+} from "../ui/sheet";
+import { MenuIcon } from "lucide-react";
 
 const Home = () => {
-  const user = useSelector((state: RootState) => state.auth.user);
-  console.log(user);
-
-  return <div>Home</div>;
+  return (
+    <div className="flex">
+      <div className="w-[20%] hidden md:block border-r-2 h-screen fixed">
+        <LeftSidebar />
+      </div>
+      <div className="flex-1 md:ml-[20%] overflow-y-auto">
+        <div className="md:hidden">
+          <Sheet>
+            <SheetTrigger>
+              <MenuIcon />
+            </SheetTrigger>
+            <SheetContent>
+              <SheetTitle></SheetTitle>
+              <SheetDescription></SheetDescription>
+              <LeftSidebar />
+            </SheetContent>
+          </Sheet>
+        </div>
+        <Feed />
+      </div>
+      <div className="w-[30%] pt-8 px-6 lg:block hidden">
+        <RightSidebar />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/components/Home/LeftSidebar.tsx
@@ -0,0 +1,119 @@
+"use client";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  Heart,
+  Home,
+  LogOutIcon,
+  MessageCircle,
+  Search,
+  SquarePlus,
+} from "lucide-react";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../../store/store";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+import { BASE_API_URL } from "../../../server";
+import { setAuthUser } from "../../../store/authSlice";
+import { toast } from "sonner";
+
+const LeftSidebar = () => {
+  const user = useSelector((state: RootState) => state.auth.user);
+  const router = useRouter();
+  const dispatch = useDispatch();
+
+  const handleLogout = async () => {
+    await axios.post(
+      `${BASE_API_URL}/users/logout`,
+      {},
+      { withCredentials: true }
+    );
+    dispatch(setAuthUser(null));
+    toast.success("Logout Successfully");
+    router.push("/auth/login");
+  };
+
+  const handleSidebar = (label: string) => {
+    if (label === "Home") {
+      router.push("/");
+    }
+    if (label === "Logout") {
+      handleLogout();
+    }
+  };
+
+  const SidebarLinks = [
+    {
+      icon: <Home />,
+      label: "Home",
+    },
+    {
+      icon: <Search />,
+      label: "Search",
+    },
+    {
+      icon: <MessageCircle />,
+      label: "Message",
+    },
+    {
+      icon: <Heart />,
+      label: "Notification",
+    },
+    {
+      icon: <SquarePlus />,
+      label: "Create",
+    },
+    {
+      icon: (
+        <Avatar className="w-9 h-9">
+          <AvatarImage src={user?.profilePicture} className="h-full w-full" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      ),
+      label: "Profile",
+    },
+    {
+      icon: <LogOutIcon />,
+      label: "Logout",
+    },
+  ];
+  return (
+    <div className="h-full">
+      <div className="lg:p-6 p-3 cursor-pointer">
+        <div
+          onClick={() => {
+            router.push("/");
+          }}
+        >
+          <Image
+            src="/images/logo.png"
+            alt="Logo"
+            width={150}
+            height={150}
+            className="mt-[-2rem]"
+          />
+        </div>
+        <div className="mt-6">
+          {SidebarLinks.map((link) => {
+            return (
+              <div
+                key={link.label}
+                className="flex items-center mb-2 p-3 rounded-lg group cursor-pointer transition-all duration-200 hover:bg-gray-100 space-x-2"
+                onClick={() => handleSidebar(link.label)}
+              >
+                <div className="group-hover:scale-110 transition-all duration-200">
+                  {link.icon}
+                </div>
+                <p className="lg:text-lg text-base">{link.label}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LeftSidebar;
