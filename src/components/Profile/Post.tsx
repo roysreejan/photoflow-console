b75d78a2a62c8f51961233dc8faa97d6817a39ee
roysreejan <sreejanroy01@gmail.com>
2025-06-21 23:21:33 +0600

    profile post completed

--- src/components/Profile/Post.tsx
+++ src/components/Profile/Post.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { User } from "../../../types";
+import Image from "next/image";
+import { Heart, MessageCircle } from "lucide-react";
 
 type Props = {
   userProfile: User | undefined;
@@ -8,7 +10,35 @@ type Props = {
 const Post = ({ userProfile }: Props) => {
   // console.log(userProfile);
 
-  return <div>Post</div>;
+  return (
+    <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+      {userProfile?.posts?.map((post) => {
+        return (
+          <div key={post._id} className="relative group overflow-hidden">
+            <Image
+              src={`${post?.image?.url}`}
+              alt="Post"
+              width={300}
+              height={300}
+              className="w-full h-full object-cover aspect-square"
+            />
+            <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+              <div className="flex space-x-6">
+                <button className="p-2 rounded-full text-white space-x-2 flex items-center font-bold">
+                  <Heart className="w-7 h-7"  />
+                  <span>{post?.likes.length}</span>
+                </button>
+                <button className="p-2 rounded-full text-white space-x-2 flex items-center font-bold">
+                  <MessageCircle className="w-7 h-7"  />
+                  <span>{post?.comments.length}</span>
+                </button>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Post;
