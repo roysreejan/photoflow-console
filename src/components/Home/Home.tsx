a3f679d9c10a4815e6b77bd0047a0199e419c0f1
roysreejan <sreejanroy01@gmail.com>
2025-06-21 22:50:14 +0600

    login access added

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import LeftSidebar from "./LeftSidebar";
 import Feed from "./Feed";
 import RightSidebar from "./RightSidebar";
@@ -11,9 +11,49 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "../ui/sheet";
-import { MenuIcon } from "lucide-react";
+import { MenuIcon, Loader } from "lucide-react";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../../store/store";
+import axios from "axios";
+import { BASE_API_URL } from "../../../server";
+import { handleAuthRequest } from "../utils/apiRequest";
+import { setAuthUser } from "../../../store/authSlice";
+import { redirect } from "next/navigation";
 
 const Home = () => {
+  const dispatch = useDispatch();
+  const user = useSelector((state: RootState) => state.auth.user);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const getAuthUser = async () => {
+      const getAuthUSerReq = async () =>
+        await axios.get(`${BASE_API_URL}/users/me`, {
+          withCredentials: true,
+        });
+      const result = await handleAuthRequest(getAuthUSerReq, setIsLoading);
+
+      if (result) {
+        dispatch(setAuthUser(result.data.data.user));
+      }
+    };
+    getAuthUser();
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (!user) {
+      return redirect("/auth/login");
+    }
+  }, [user]);
+
+  if (isLoading) {
+    return (
+      <div className="w-full h-screen flex items-center justify-center flex-col">
+        <Loader className="animate-spin" />
+      </div>
+    );
+  }
+
   return (
     <div className="flex">
       <div className="w-[20%] hidden md:block border-r-2 h-screen fixed">
